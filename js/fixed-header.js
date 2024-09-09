8be3c9108757d33f92ed622d068a82150c2ebd2f
Mike <mg.korop@gmail.com>
2024-09-09 18:08:53 +0300

    scss structure done, started working on header

---
+++ js/fixed-header.js
@@ -0,0 +1,4 @@
+const header = document.getElementById('header');
+const headerHeight = header.offsetHeight;
+const body = document.getElementById('body');
+body.style.paddingTop = headerHeight + 'px';
