2091bd5c33a25ec059c0e1d9024686695a2c1ab0
Mike <mg.korop@gmail.com>
2024-10-02 20:42:32 +0300

    hero bg done, minor fixes

--- js/fixed-header.js
+++ js/fixed-header.js
@@ -1,4 +1,4 @@
 const header = document.getElementById('header');
-const headerHeight = header.offsetHeight;
+const headerHeight = header.offsetHeight - 1;
 const body = document.getElementById('body');
 body.style.paddingTop = headerHeight + 'px';
